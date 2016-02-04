b90cacc0090e872d76d3f134dd1b22e40e4e2756
mreverter <revertermauro@gmail.com>
2016-02-04 14:16:34 -0300

    documentation added

--- sdk/src/main/java/com/mercadopago/core/Settings.java
+++
@@ -1,8 +0,0 @@
-package com.mercadopago.core;
-
-import retrofit.RestAdapter;
-
-public class Settings {
-
-    public static final retrofit.RestAdapter.LogLevel RETROFIT_LOGGING = RestAdapter.LogLevel.NONE;
-}
