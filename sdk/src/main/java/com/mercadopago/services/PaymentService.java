3fab23d64f6a13d30931b22867809e3832cf8ff4
Vale Serber <valserber@gmail.com>
2016-06-10 16:22:11 -0300

    changed to v1

--- sdk/src/main/java/com/mercadopago/services/PaymentService.java
+++ sdk/src/main/java/com/mercadopago/services/PaymentService.java
@@ -25,7 +25,7 @@ public interface PaymentService {
     @GET("/v1/payment_methods")
     MPCall<List<PaymentMethod>> getPaymentMethods(@Query("public_key") String publicKey);
 
-    @GET("/beta/checkout/payment_methods/search/options")
+    @GET("/v1/checkout/payment_methods/search/options")
     MPCall<PaymentMethodSearch> getPaymentMethodSearch(@Query("public_key") String publicKey, @Query("amount") BigDecimal amount, @Query("excluded_payment_types") String excludedPaymentTypes, @Query("excluded_payment_methods") String excludedPaymentMethods);
 
     @GET("/v1/payment_methods/installments")
@@ -34,12 +34,12 @@ public interface PaymentService {
     @GET("/v1/payment_methods/card_issuers")
     MPCall<List<Issuer>> getIssuers(@Query("public_key") String publicKey, @Query("payment_method_id") String paymentMethodId, @Query("bin") String bin);
 
-    @POST("/beta/checkout/payments")
+    @POST("/v1/checkout/payments")
     MPCall<Payment> createPayment(@Header("X-Idempotency-Key") String transactionId, @Body PaymentIntent body);
 
-    @GET("/beta/checkout/payments/{payment_id}/results")
+    @GET("/v1/checkout/payments/{payment_id}/results")
     MPCall<Instruction> getInstruction(@Path(value = "payment_id", encoded = true) Long paymentId, @Query("public_key") String mKey, @Query("payment_type") String paymentTypeId);
 
-    @GET("/beta/checkout/preferences/{preference_id}")
+    @GET("/v1/checkout/preferences/{preference_id}")
     MPCall<CheckoutPreference> getPreference(@Path(value = "preference_id", encoded = true) String checkoutPreferenceId, @Query("public_key") String publicKey);
 }
