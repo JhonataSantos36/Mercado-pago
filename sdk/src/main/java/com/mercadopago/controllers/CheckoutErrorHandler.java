36f2511e0a52f1b8fa90ff4fa9e4aad748877007
Mauro Reverter <revertermauro@gmail.com>
2016-11-25 14:47:58 -0300

    add custom error screen (#207)

---
+++ sdk/src/main/java/com/mercadopago/controllers/CheckoutErrorHandler.java
@@ -0,0 +1,45 @@
+package com.mercadopago.controllers;
+
+import android.support.annotation.LayoutRes;
+
+/**
+ * Created by mreverter on 11/25/16.
+ */
+
+public class CheckoutErrorHandler {
+
+    private static CheckoutErrorHandler checkoutErrorHandler;
+    private Integer customErrorLayout;
+
+    public static CheckoutErrorHandler getInstance() {
+        if (checkoutErrorHandler == null) {
+            checkoutErrorHandler = new CheckoutErrorHandler();
+        }
+        return checkoutErrorHandler;
+    }
+
+    /**
+     *
+     * @param layout
+     * Add a layout to show when an error occurs.
+     * The layout MUST have the following views:
+     *   TextView with ID: mpsdkErrorMessage - Message shown to the user.
+     *   View with ID: mpsdkErrorRetry - Any view giving the user the chance to retry.
+     *   View with ID: mpsdkExit - Any view that will cancel the action in progress.
+     */
+    public void setCustomErrorLayout(@LayoutRes int layout) {
+        this.customErrorLayout = layout;
+    }
+
+    public void setGeneric(@LayoutRes int layout) {
+        this.customErrorLayout = layout;
+    }
+
+    public boolean hasCustomErrorLayout() {
+        return customErrorLayout != null;
+    }
+
+    public Integer getCustomErrorLayout() {
+        return customErrorLayout;
+    }
+}
