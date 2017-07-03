549aae37d299463d93e63242f8cd935b4386b3fe
valeserber <valserber@gmail.com>
2017-07-03 11:01:18 -0300

    Updated documentation from beta to v3.1.0 (#577)
    * updated documentation from beta to v3.1.0
    
    * improved docu
    
    * changed mercadopago to mercado pago
    
    * adding supported sites
    
    * updated english files
    
    * disclaimer in english version 3

---
+++ js/versions.js
@@ -0,0 +1,10 @@
+var old_v2_version = "2.5.4";
+var new_v3_version = "3.1.0";
+var old_v2_list = document.getElementsByClassName("oldV2Version");
+var new_v3_list = document.getElementsByClassName("newV3Version");
+for (i = 0; i < old_v2_list.length; i++) {
+  old_v2_list[i].innerHTML = old_v2_version;
+}
+for (i = 0; i < new_v3_list.length; i++) {
+  new_v3_list[i].innerHTML = new_v3_version;
+}
