8f9e6083ec182903060a80e1b164ddbae3621be7
valeserber <valserber@gmail.com>
2018-01-29 18:28:54 -0300

    [Fix] Added status details rejected in payment processor for ESC (#824)
    * added status details rejected in payment processor
    
    * changed long to Long to avoid crash in payment without id

--- sdk/src/main/java/com/mercadopago/plugins/model/ProcessorPaymentResult.java
+++ sdk/src/main/java/com/mercadopago/plugins/model/ProcessorPaymentResult.java
@@ -11,12 +11,12 @@ import com.mercadopago.model.PaymentData;
 
 public class ProcessorPaymentResult {
 
-    public final long paymentId;
+    public final Long paymentId;
     public final String status;
     public final String statusDetail;
     public final PaymentData paymentData;
 
-    public ProcessorPaymentResult(final long paymentId,
+    public ProcessorPaymentResult(final Long paymentId,
                                   final @NonNull String status,
                                   final @NonNull String statusDetail,
                                   final @NonNull PaymentData paymentData) {
@@ -36,7 +36,14 @@ public class ProcessorPaymentResult {
                     || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_BAD_FILLED_DATE.equals(statusDetail)
                     || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_CARD_DISABLED.equals(statusDetail)
                     || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_BAD_FILLED_CARD_NUMBER.equals(statusDetail)
-                    || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_CALL_FOR_AUTHORIZE.equals(statusDetail)) {
+                    || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_CALL_FOR_AUTHORIZE.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_DUPLICATED_PAYMENT.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_INSUFFICIENT_AMOUNT.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_MAX_ATTEMPTS.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_INVALID_ESC.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_REJECTED_HIGH_RISK.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_REJECTED_REJECTED_BY_BANK.equals(statusDetail)
+                    || Payment.StatusCodes.STATUS_DETAIL_REJECTED_REJECTED_INSUFFICIENT_DATA.equals(statusDetail)) {
                 return statusDetail;
             } else {
                 return Payment.StatusCodes.STATUS_DETAIL_CC_REJECTED_PLUGIN_PM;
