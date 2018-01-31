8a8996007b6f0b17285fe45b53721b991e8f51b5
Nicolas Fortunato <nicolas.fortunato@mercadolibre.com>
2018-01-31 18:26:38 -0300

    version beta 14

--- sdk/src/main/java/com/mercadopago/services/CheckoutService.java
+++ sdk/src/main/java/com/mercadopago/services/CheckoutService.java
@@ -24,15 +24,15 @@ import retrofit2.http.Query;
 
 public interface CheckoutService {
 
-    @POST("/" + BuildConfig.API_VERSION + "/checkout/payment_methods/search/options")
+    @POST("/v1/checkout/payment_methods/search/options")
     MPCall<PaymentMethodSearch> getPaymentMethodSearch(@Header("Accept-Language") String locale, @Query("public_key") String publicKey, @Query("amount") BigDecimal amount, @Query("excluded_payment_types") String excludedPaymentTypes, @Query("excluded_payment_methods") String excludedPaymentMethods, @Body PayerIntent payerIntent, @Query("site_id") String siteId, @Query("api_version") String apiVersion, @Query("processing_mode") String processingMode);
 
-    @POST("/" + BuildConfig.API_VERSION + "/checkout/payments")
+    @POST("/v1/checkout/payments")
     MPCall<Payment> createPayment(@Header("X-Idempotency-Key") String transactionId, @Body PaymentBody body);
 
-    @GET("/" + BuildConfig.API_VERSION + "/checkout/payments/{payment_id}/results")
+    @GET("/v1/checkout/payments/{payment_id}/results")
     MPCall<Instructions> getPaymentResult(@Header("Accept-Language") String locale, @Path(value = "payment_id", encoded = true) Long paymentId, @Query("public_key") String mKey,  @Query("access_token") String privateKey, @Query("payment_type") String paymentTypeId, @Query("api_version") String apiVersion);
 
-    @GET("/" + BuildConfig.API_VERSION + "/checkout/preferences/{preference_id}")
+    @GET("/v1/checkout/preferences/{preference_id}")
     MPCall<CheckoutPreference> getPreference(@Path(value = "preference_id", encoded = true) String checkoutPreferenceId, @Query("public_key") String publicKey);
 }
