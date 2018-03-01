9fbf135be4c4d439f7fdcf2c0d60bb921bf38085
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2018-03-01 14:04:57 -0300

    ReviewAndConfirmRenderer with new render (parent)

--- sdk/src/main/java/com/mercadopago/review_and_confirm/components/ReviewAndConfirmRenderer.java
+++ sdk/src/main/java/com/mercadopago/review_and_confirm/components/ReviewAndConfirmRenderer.java
@@ -1,7 +1,10 @@
 package com.mercadopago.review_and_confirm.components;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.mercadopago.components.Renderer;
@@ -11,9 +14,11 @@ import com.mercadopago.components.Renderer;
  */
 
 public class ReviewAndConfirmRenderer extends Renderer<ReviewAndConfirmContainer> {
+
     @Override
-    public View render(final ReviewAndConfirmContainer component, final Context context) {
-        final TextView view = new TextView(context);
-        return view;
+    protected View render(@NonNull final ReviewAndConfirmContainer component,
+                          @NonNull final Context context,
+                          @Nullable final ViewGroup parent) {
+        return null;
     }
 }
