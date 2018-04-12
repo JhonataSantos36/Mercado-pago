716494338e5779670c1fc4dbd8dd1101fa5904f2
AngieArlanti <angeles.arlanti@mercadolibre.com>
2018-04-12 14:29:21 -0300

    [Fix] Codacy fixes
    * package names without camelcase.
    * switch without returns.

---
+++ testcheckout/src/main/java/com/mercadopago/testCheckout/input/Visa.java
@@ -0,0 +1,42 @@
+package com.mercadopago.testCheckout.input;
+
+
+public class Visa extends FakeCard {
+
+    public Visa(final CardState cardState, final Country country) {
+        super(cardState, getNumber(country));
+    }
+
+    private static String getNumber(Country country) {
+        String cardNumber = null;
+
+        switch (country) {
+            case ARGENTINA:
+                cardNumber = "4509953566233704";
+                break;
+            case BRASIL:
+                cardNumber = "4235647728025682";
+                break;
+            case CHILE:
+                cardNumber = "4168818844447115";
+                break;
+            case COLOMBIA:
+                cardNumber = "4013540682746260";
+                break;
+            case MEXICO:
+                cardNumber = "4075595716483764";
+                break;
+            case PERU:
+                cardNumber = "4009175332806176";
+                break;
+            case URUGUAY:
+                cardNumber = "4014682387532428";
+                break;
+            case VENEZUELA:
+                cardNumber = "4966382331109310";
+                break;
+        }
+
+        return cardNumber;
+    }
+}
