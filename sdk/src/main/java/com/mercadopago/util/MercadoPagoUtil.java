5d689b62c0cc2ca7eb29fdedec880d747a1f6295
AngieArlanti <angeles.arlanti@mercadolibre.com>
2018-04-13 15:30:48 -0300

    [Fix] Lint error: Plurals Candidate (#945)
    * [Fix] lintOptions abortOnError false for all modules
    
    * [Fix] Plurals Candidate lint error fixed
    
    * Removed unused methods.
    * Removed unused and bad-formed strings.

--- sdk/src/main/java/com/mercadopago/util/MercadoPagoUtil.java
+++ sdk/src/main/java/com/mercadopago/util/MercadoPagoUtil.java
@@ -21,11 +21,6 @@ public class MercadoPagoUtil {
         return getPaymentMethodPicture(context, SDK_PREFIX, paymentMethodId);
     }
 
-    public static int getPaymentMethodImage(Context context, String paymentMethodId) {
-
-        return getPaymentMethodPicture(context, SDK_PREFIX + "img_tc_", paymentMethodId);
-    }
-
     private static int getPaymentMethodPicture(Context context, String type, String paymentMethodId) {
 
         int resource;
@@ -54,34 +49,6 @@ public class MercadoPagoUtil {
             resource = 0;
         }
         return resource;
-    }
-
-    public static String getCVVDescriptor(Context context, PaymentMethod paymentMethod) {
-
-        if ("amex".equals(paymentMethod.getId())) {
-            return String.format(context.getString(com.mercadopago.R.string.mpsdk_cod_seg_desc_amex), 4);
-        } else {
-            return String.format(context.getString(com.mercadopago.R.string.mpsdk_cod_seg_desc), 3);
-        }
-    }
-
-    public static int getCVVImageResource(Context context, PaymentMethod paymentMethod) {
-
-        return getPaymentMethodImage(context, paymentMethod.getId());
-    }
-
-    public static String formatDate(Context context, Date date) {
-
-        String result;
-        try {
-            result = new SimpleDateFormat("dd MM yyyy HH:mm").format(date);
-            String[] splitString = result.split(" ");
-            result = context.getString(R.string.mpsdk_format_date, splitString[0], splitString[1], splitString[2], splitString[3]);
-        } catch (Exception ex) {
-            // do nothing
-            result = ex.getMessage();
-        }
-        return result;
     }
 
     public static boolean isCard(String paymentTypeId) {
